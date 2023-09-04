bc3ef1679d1da3ca4bbb31b110cd6be8ec935622
Hendrik Scholtz <schwarzbaer@gmx.biz>
2023-09-04 11:19:37 +0200

    feat: added withers to Animal

--- src/main/java/org/example/records/Animal.java
+++ src/main/java/org/example/records/Animal.java
@@ -1,4 +1,10 @@
 package org.example.records;
 
+@SuppressWarnings("unused")
 public record Animal(String id, String name, Species species, double age, Owner owner) {
+    public Animal withID     ( String  id      ) { return new Animal(id, name, species, age, owner); }
+    public Animal withName   ( String  name    ) { return new Animal(id, name, species, age, owner); }
+    public Animal withSpecies( Species species ) { return new Animal(id, name, species, age, owner); }
+    public Animal withAge    ( double  age     ) { return new Animal(id, name, species, age, owner); }
+    public Animal withOwner  ( Owner   owner   ) { return new Animal(id, name, species, age, owner); }
 }
