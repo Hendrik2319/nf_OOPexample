a3351c3f94c7e4efc934d302ab293469e809469f
Hendrik Scholtz <schwarzbaer@gmx.biz>
2023-09-07 11:31:52 +0200

    feat: added bonus task about "Streams"

---
+++ src/main/java/org/example/streams/StudentsCSV.java
@@ -0,0 +1,62 @@
+package org.example.streams;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+public class StudentsCSV {
+/*
+	•	Schaue dir die Datei students.csv an. Kopiere die Datei in das Root-Verzeichnis deines Projektes.
+	     ->     https://neuefische.github.io/java-curriculum/3-Ecosystem/03-Stream/res/students.csv
+	•	Nutze Streams, um die Datei zeilenweise einzulesen und auszugeben (Tipp: Files.lines(Path.of("students.csv")))
+	•	Entferne dabei die Überschrift
+	•	Wandel die Zeilen jeweils in die Klasse Student um
+	•	Entferne ungültige Zeilen und Duplikate
+ */
+    public static void main(String[] args) {
+        try (Stream<String> lines = Files.lines(Path.of("students.csv"))) {
+
+            lines
+                    .filter(str -> !str.isBlank() && !str.startsWith("ID,"))
+                    .distinct()
+                    .map(Student::parse)
+                    .forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private record Student(int id, String name, int zipCode, int age) {
+
+        static Student parse(String str) {
+            if (str==null) return null;
+
+            String[] parts = str.split(",");
+            if (parts.length!=4) { System.out.printf("Line has wrong number of fields: \"%s\"%n", str); return null; }
+
+            Integer id = parseInt(parts[0]);
+            if (id==null) { System.out.printf("Can't parse [id] field: \"%s\" in \"%s\"%n", parts[0], str); return null; }
+
+            String name = parts[2];
+
+            Integer zipCode = parseInt(parts[2]);
+            if (zipCode==null) { System.out.printf("Can't parse [zipCode] field: \"%s\" in \"%s\"%n", parts[2], str); return null; }
+
+            Integer age = parseInt(parts[3]);
+            if (age==null) { System.out.printf("Can't parse [age] field: \"%s\" in \"%s\"%n", parts[2], str); return null; }
+
+            return new Student(id, name, zipCode, age);
+        }
+
+        private static Integer parseInt(String str) {
+            try {
+                return Integer.parseInt(str);
+            } catch (NumberFormatException e) {
+                return null;
+            }
+        }
+
+    }
+}
