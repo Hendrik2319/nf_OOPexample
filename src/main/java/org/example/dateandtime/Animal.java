fefef2ae627d8b097a2ed33725d77f7f6f5577ed
Hendrik Scholtz <schwarzbaer@gmx.biz>
2023-09-11 15:59:52 +0200

    feat: solved bonus task of "Date & Time"
    and some beautifying of outputs

---
+++ src/main/java/org/example/dateandtime/Animal.java
@@ -0,0 +1,6 @@
+package org.example.dateandtime;
+
+import java.time.LocalDate;
+
+public record Animal(String id, String name, LocalDate dateOfBirth) {
+}
--- src/main/java/org/example/dateandtime/Main.java
+++ src/main/java/org/example/dateandtime/Main.java
@@ -1,27 +1,36 @@
 package org.example.dateandtime;
 
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
+import java.time.*;
 import java.time.temporal.ChronoUnit;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        // default
         ZonedDateTime now = ZonedDateTime.now();
-        System.out.println(now);
+        System.out.println("now       : "+ now);
 
         ZonedDateTime in2Weeks = ChronoUnit.WEEKS.addTo(now, 2);
-        System.out.println(in2Weeks);
+        System.out.println("in 2 Weeks: "+ in2Weeks);
 
         System.out.println();
         ZonedDateTime sometime = ZonedDateTime.of(2023,10, 10,0,0,0,0, ZoneId.systemDefault());
         int cmp = now.compareTo(sometime);
-        System.out.printf("Time 1: %s%n   %s%nTime 2: %s%n", now, cmp==0 ? "is equal to" : cmp<0 ? "is before" : "is after", sometime);
+        System.out.println("Time 1: "+ now);
+        System.out.println("   "+ (cmp==0 ? "is equal to" : cmp<0 ? "is before" : "is after"));
+        System.out.println("Time 2: "+ sometime);
 
-        System.out.println("Difference: "+ ChronoUnit.DAYS.between(now, sometime));
+        System.out.println("Difference: "+ ChronoUnit.DAYS.between(now, sometime)+" day(s)");
+        System.out.println();
 
+        // bonus
+        Animal anim1 = new Animal("anim1", "Animal 1", LocalDate.of(2019, 9, 11));
+        System.out.println("Anim1: "+anim1);
+        long days = computeDaysUntilNextBirthday(anim1.dateOfBirth());
+        System.out.println("Days until next birthday: "+days);
+
+        // experimenting
 
         System.out.println();
         try {
@@ -36,5 +45,13 @@ public class Main {
             System.out.println("Fehler 2: "+e.getMessage());
         }
 
+    }
+
+    private static long computeDaysUntilNextBirthday(LocalDate dateOfBirth) {
+        LocalDate now = LocalDate.now();
+        LocalDate nextBirthday = dateOfBirth.withYear(now.getYear());
+        while (now.isAfter(nextBirthday))
+            nextBirthday = ChronoUnit.YEARS.addTo(nextBirthday,1);
+        return ChronoUnit.DAYS.between(now,nextBirthday);
     }
 }
